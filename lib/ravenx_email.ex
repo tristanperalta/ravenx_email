c5c5b77d787084eb42ef0d5a7330f53240520c2d
Óscar de Arriba <odarriba@gmail.com>
2018-02-17 11:45:25 +0100

    Base structure

---
+++ lib/ravenx_email.ex
@@ -0,0 +1,18 @@
+defmodule RavenxEmail do
+  @moduledoc """
+  Documentation for RavenxEmail.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RavenxEmail.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ravenx_email_test.exs
@@ -0,0 +1,8 @@
+defmodule RavenxEmailTest do
+  use ExUnit.Case
+  doctest RavenxEmail
+
+  test "greets the world" do
+    assert RavenxEmail.hello() == :world
+  end
+end
