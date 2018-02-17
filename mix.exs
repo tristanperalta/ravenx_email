7d884bfab28c903ef8a182b3cc5e88625722e367
Óscar de Arriba <odarriba@gmail.com>
2018-02-17 11:49:26 +0100

    Update mix file

--- mix.exs
+++ mix.exs
@@ -5,9 +5,13 @@ defmodule RavenxEmail.MixProject do
     [
       app: :ravenx_email,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -24,6 +28,31 @@ defmodule RavenxEmail.MixProject do
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:mock, "~> 0.3.0", only: :test},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: "https://github.com/acutario/ravenx_email",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp description do
+    """
+    Ravenx strategy to send notifications using e-mail.
+    """
+  end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      name: :ravenx_slack,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Óscar de Arriba"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/acutario/ravenx_email"}
     ]
   end
 end
